5ec4709db49601be3170a7e907cb550726777229
Mr.Nobody <id4acc2014@protonmail.com>
2024-11-03 00:32:40 +0330

    feat: :sparkles: i enhanced price filtering logic in search bar component
    i enhanced the SearchBar component in components/module/SearchBar.js to manage minimum and maximum price inputs using local state with useState.
    the searchHandler function routes to a filter page based on the entered prices, ensuring both fields are filled before navigating.
    if the fields are empty, an alert prompts the user to enter the required values.

--- components/module/SearchBar.js
+++ components/module/SearchBar.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
+import { useRouter } from "next/router";
+
 function SearchBar() {
+  const router = useRouter();
+
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+
+  const searchHandler = () => {
+    if (min && max) {
+      router.push(`/filter/${min}/${max}`);
+    } else {
+      alert("Please enter minimum and maximum price");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div>
-        <input type="text" placeholder="minimum price" />
-        <input type="text" placeholder="maximum price" />
+        <input type="text" placeholder="minimum price" value={min} onChange={(e) => setMin(e.target.value)} />
+        <input type="text" placeholder="maximum price" value={max} onChange={(e) => setMax(e.target.value)} />
       </div>
-      <button>Search</button>
+      <button onClick={searchHandler}>Search</button>
     </div>
   );
 }

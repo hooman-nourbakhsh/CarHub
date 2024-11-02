9447eff8d2bff59dededcb6f7768d83705a52f01
Mr.Nobody <id4acc2014@protonmail.com>
2024-11-02 21:46:28 +0330

    feat: :sparkles: i added cars data and display component for car listings
    i added a public/images directory with car images and created a data/carsData.js file with detailed information about each car, including name, model, year, price, and description.
    i then created a CarsPage component in components/templates/CarsPage.js, displaying car names using mapped data.
    after adding css modules for styling, i integrated the component into a new index.js file in pages/cars, where it imports and displays data from carsData.js.

---
+++ pages/cars/index.js
@@ -0,0 +1,11 @@
+import CarsPage from "../../components/templates/CarsPage";
+import carsData from "../../data/carsData";
+
+function Details() {
+  return (
+    <div>
+      <CarsPage data={carsData} />
+    </div>
+  );
+}
+export default Details;
